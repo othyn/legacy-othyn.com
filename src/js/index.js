dd7f20ef5526f3861b2614f01dd06bebc0f45d70
othyn <benny369@hotmail.co.uk>
2017-01-14 12:49:49 +0000

    Library Additions

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
+// NPM - CSS - Lib
+require('animate.css/animate.css');
+
+// LOCAL - JS - Lib
+require('script-loader!./scramble.lib.js');
+
+// LOCAL - SCSS - Project
 require('../scss/indexComponent.scss');
