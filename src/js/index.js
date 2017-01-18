a2375d93a8681593c690762a7314706838b72e27
othyn <benny369@hotmail.co.uk>
2017-01-18 22:26:00 +0000

    UglifyJS doesn't speak your new fangled ES6

--- src/js/index.js
+++ src/js/index.js
@@ -9,10 +9,10 @@ require('script-loader!./scramble.lib.js');
 require('../scss/indexComponent.scss');
 
 // Text Scramble Init: http://codepen.io/soulwire/pen/mErPAK
-let phrases = ['Hi, I\'m Ben', 'or often Othyn', 'I like to build stuff', 'Explore stuff', 'Learn stuff', 'Just lots of stuff really'];
-let Scramble = new TextScramble(document.querySelector('#scramble'));
-let counter = 0;
-let next = function next() {
+var phrases = ['Hi, I\'m Ben', 'or often Othyn', 'I like to build stuff', 'Explore stuff', 'Learn stuff', 'Just lots of stuff really'];
+var Scramble = new TextScramble(document.querySelector('#scramble'));
+var counter = 0;
+var next = function next() {
 	Scramble.setText(phrases[counter]).then(function () {
 		setTimeout(next, 3000);
 	});
